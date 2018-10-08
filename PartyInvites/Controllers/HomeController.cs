7a4faf1fae0d39300a890829dd73b31c635a9ac0
trangtran234 <tn-trang.tran@outlook.com.vn>
2018-10-08 16:47:24 +0700

    Add HomeController for practicing and understanding routes

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -8,9 +8,9 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello World";
+            return View();
         }
     }
 }
